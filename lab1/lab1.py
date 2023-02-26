e3f3bd5a2ffc53675627b2a00220ef4b6e325275
IvanKovalov <kovalevvana8@gmail.com>
2023-02-26 14:56:38 +0300

    add all commands

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -2,16 +2,20 @@ import imutils
 import numpy as np
 import cv2
 img = cv2.imread ('foto.jpg')
-cv2.imshow("My img", img)
+#cv2.imshow("My img", img)
 output = img.copy()
 #cv2.rectangle(output, (270, 50), (890, 560), (0, 120, 255), 2)
 #cv2 . circle (output, (600, 300), 275, (100, 140, 255), 2)
 #resized = imutils.resize(img, height=300)
 #rotated = imutils.rotate(img, -45)
+#blurred = cv2.GaussianBlur (img, (11, 11), 0)
+#cv2.imshow ("Blurred", blurred)
 
-font2 =cv2 . FONT_HERSHEY_SCRIPT_COMPLEX
-cv2 . putText (output, 'First time',(10, 500), font2, 4,(255, 255, 255),2 ,cv2.LINE_4)
-cv2.imshow ("Rectangle", output)
+#font2 =cv2 . FONT_HERSHEY_SCRIPT_COMPLEX
+#cv2 . putText (output, 'First time',(10, 500), font2, 4,(255, 255, 255),2 ,cv2.LINE_4)
+cv2.line(img,(300, 0),(300, 800),(0,100, 255),5)
+
+cv2.imshow ("Rectangle", img)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
